1204d1d355fe8c068cd1c588942a1e2a608deee0
Logan Delafosse <bioready@gmail.com>
2022-01-15 16:56:55 -0600

    build: free mints work, non-free mint works, supply cap works, tokenURI works, reveal works.. working on tokenURI for post-reveal

---
+++ contracts/FrankenPhunks.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FrankenPhunks is ERC721Enumerable, ReentrancyGuard, Ownable {
+
+    using Strings for uint256;
+
+
+// *****  GLOBAL STATE *****
+
+  uint16 public constant MAX_SUPPLY = 3333;
+  uint16 public constant FREE_MINTS = 1111;
+  uint8 public constant MAX_MULTIMINT = 7;
+  uint256 public constant PRICE = 0.032 ether;
+
+
+  using Counters for Counters.Counter;
+  bool public revealed = false;
+  string public notRevealedUri;
+  string baseURI;
+  string public baseExtension = ".json";
+
+
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initNotRevealedUri
+  ) ERC721(_name, _symbol) {
+    setNotRevealedURI(_initNotRevealedUri);
+  }
+  
+
+// ***** MINTING *****
+
+  function mint(uint16 count) public payable nonReentrant {
+
+    require(saleIsActive, "Sale not active");
+    require(totalSupply() + count - 1 < MAX_SUPPLY, "Exceeds max supply");
+    require(count <= MAX_MULTIMINT, "Mint at most 7 at a time");
+
+      // Free Mints
+    if (totalSupply() <= FREE_MINTS) {
+        for (uint256 i = 0; i < count; i++) {
+        _mint(msg.sender, totalSupply());
+      }
+    }
+
+
+      // Mint
+    if (totalSupply() > FREE_MINTS) {
+
+    require(
+      msg.value >= PRICE * count, "Insufficient payment, 0.032 ETH per item"
+    );
+        for (uint256 i = 0; i < count; i++) {
+        _mint(msg.sender, totalSupply());
+      }
+    }
+  }
+
+// ***** ACTIVATION *****
+
+  bool public saleIsActive = true;
+
+  function setSaleIsActive(bool saleIsActive_) external onlyOwner {
+    saleIsActive = saleIsActive_;
+  }
+
+// ***** REVEAL *****
+
+  //only owner  
+  function reveal() public onlyOwner() {
+      revealed = true;
+  }
+  
+
+// ***** URI HANDLING *****
+
+  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+    notRevealedUri = _notRevealedURI;
+  }
+
+  function setBaseURI(string memory _newBaseURI) external onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+    if(revealed == false) {
+      return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, "{", _tokenId.toString(), "}"))
+        : "";
+  }
+
+// ***** PAYOUT *****
+
+  function withdraw() public nonReentrant {
+    uint256 balance = address(this).balance;
+
+    Address.sendValue(payable(owner()), balance);
+  }
+}
---
+++ test/test-phunks.js
@@ -0,0 +1,47 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Deploy & Test FrankenPhunks", function () {
+
+  let accounts, FrankenPhunks, contract, deployer, frankenPhunksContract
+
+  beforeEach('Set up contract for each test', async () => {
+    FrankenPhunks = await hre.ethers.getContractFactory("FrankenPhunks");
+    frankenPhunksContract = await FrankenPhunks.deploy("2DFrankenPhunks", "FRPHUNKS", "ipfs://bafybeiagkivpeohoid23ntzy4rkurqbvtrqwuqgirbhplz7k74whnxojte/?filename=waveicon-32x32.png");
+    contract = await frankenPhunksContract.deployed();
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+  })
+
+  it("Mints a new token", async function () {
+    const cost = ethers.utils.parseEther("0.096");
+    await frankenPhunksContract.deployed();
+    console.log("Contract address:", contract.address)
+
+
+    const balanceBefore = await contract.balanceOf(deployer.address);
+    console.log("Balance of deployer", balanceBefore.toString());
+
+    await contract.setBaseURI("ipfs://bafybeiagkivpeohoid23ntzy4rkurqbvtrqwuqgirbhplz7k74whnxojte/?filename=")
+
+    await contract.mint(3);
+
+
+    balanceAfter = await contract.balanceOf(deployer.address);
+    console.log("Balance of Deployer after mint:", balanceAfter.toString());
+
+    // Token URI Before reveal
+    console.log("Token URIs before reveal:");
+    console.log("Token 0 URI:", await contract.tokenURI(0));
+    console.log("Token 1 URI:", await contract.tokenURI(1));
+    console.log("Token 2 URI:", await contract.tokenURI(2));
+
+    console.log("Token URIs after reveal:");
+    contract.reveal();
+    console.log("Token 0 URI:", await contract.tokenURI(0));
+    console.log("Token 1 URI:", await contract.tokenURI(1));
+    console.log("Token 2 URI:", await contract.tokenURI(2));
+
+    
+  });
+});
