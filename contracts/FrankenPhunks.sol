0536f05f7e97e2ec93def5dc35359a0022bc2150
Logan Delafosse <bioready@gmail.com>
2022-01-17 14:18:56 -0600

    finalized contract

--- contracts/FrankenPhunks.sol
+++ contracts/FrankenPhunks.sol
@@ -49,7 +49,7 @@ contract FrankenPhunks is ERC721Enumerable, ReentrancyGuard, Ownable {
   function mint(uint16 count) public payable nonReentrant {
 
     require(totalSupply() + count - 1 < MAX_SUPPLY, "Exceeds max supply");
-    // require(count <= MAX_MULTIMINT, "Mint at most 7 at a time");
+    require(count <= MAX_MULTIMINT, "Mint at most 7 at a time");
 
       // Free Mints
     if (totalSupply() <= FREE_MINTS) {
